c50ce3fadab6d21fc1743a0a2408817c654df7a2
Mr-Himanshu7 <himanshuyadac@gmail.com>
2025-04-26 16:18:21 +0530

    PHP PROJECT

---
+++ logout.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>
+            logout
+        </title>
+        <style>
+            .a{
+                background-color: bisque;
+                color: purple;
+            }
+            .b{
+                font-size: 50px;
+                margin-left: 500px;
+                color: blue;
+            }
+            .c{
+                font-size: 50px;
+                margin-left: 500px;
+                color: purple;
+            }
+        </style>
+    </head>
+    <body class="a">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p class="c">Admin login page</p>
+        <a href="registrationpage.php" class="b">Login</a>
+       
+    </body>
+</html>
